d7af2e9014ed8c0bed3ede73553fb067c8e7bbe1
jz <jeffz@eml.cc>
2014-09-09 20:57:27 -0600

    resemble working, just sans ncurses for now

--- mpdvz.c
+++ mpdvz.c
@@ -1,14 +1,74 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+#ifdef __linux__
+#include <sys/ioctl.h>
+#endif
 
 #include <ncurses.h>
 
+
+int win_width, win_height, win_mode;
+
+static int _getdims(int fd) {
+#ifdef TIOCGSIZE
+    struct ttysize ts;
+    if(ioctl(fd, TIOCGSIZE, &ts)) { perror(""); return -1; }
+    if(ts.ts_cols > 0) {
+        win_width = ts.ts_cols;
+        win_height = ts.ts_lines;
+        win_mode = 1 + fd;
+        return 0;
+    }
+#elif defined(TIOCGWINSZ)
+    struct winsize ws;
+    if(ioctl(fd, TIOCGWINSZ, &ws)) { perror(""); return -1; }
+    if(ws.ws_col > 0) {
+        win_width = ws.ws_col;
+        win_height = ws.ws_row;
+        win_mode = 1 + fd;
+        return 0;
+    }
+#endif
+    return -1;
+}
+
+static void getdims() {
+    int retval, rows, cols;
+    FILE *f;
+    win_mode = 0;
+    retval = 0;
+#define GETCOLS(x) \
+    if(isatty(x) && ((retval = _getdims(x)) != -1)) return;
+    GETCOLS(STDIN_FILENO)
+    GETCOLS(STDOUT_FILENO)
+    GETCOLS(STDERR_FILENO)
+    if(getenv("COLUMNS") && getenv("LINES")) {
+        win_width = atoi(getenv("COLUMNS"));
+        win_height= atoi(getenv("LINES"));
+        if(win_width > 0 && win_height > 0) { win_mode = 4; return; }
+    }
+#define STTY(x, y) \
+    if((f = popen("stty " x " size", "r"))!=NULL) { \
+        if(fscanf(f, "%d %d", &rows, &cols) > 0 && cols>0) \
+            { win_height = rows; win_width = cols; win_mode = y; pclose(f); return; } pclose(f);\
+    }
+    STTY("-f /dev/stderr", 5)
+    STTY("", 6)
+    STTY("-f /dev/stdout", 7)
+    win_width = win_height = 0;
+    win_mode = 8;
+    return;
+}
 
 typedef struct {
     int size;
     int start;
     int end;
-    short *buf;
+    int *buf;
 } CBuf;
 
 
@@ -16,10 +76,10 @@ void cb_init(CBuf *buf, int size) {
     buf->size = size + 1;
     buf->start = 0;
     buf->end = 0;
-    buf->buf = calloc(buf->size, sizeof(short));
+    buf->buf = calloc(buf->size, sizeof(int));
 }
 
-void cb_write(CBuf *buf, short val) {
+void cb_write(CBuf *buf, int val) {
     buf->buf[buf->end] = val;
     buf->end = (buf->end + 1) % buf->size;
     if(buf->end == buf->start) {
@@ -27,13 +87,15 @@ void cb_write(CBuf *buf, short val) {
     }
 }
 
-short cb_read(CBuf *buf, int idx) {
+int cb_read(CBuf *buf, int idx) {
     return buf->buf[(buf->start + idx) % buf->size];
 }
 
 void cb_debug(CBuf *buf) {
     for(int i = 0; i < buf->size - 1; i++) {
-        printf("%d\n", cb_read(buf, i));
+        int v = cb_read(buf, i);
+        float r = (v + 32767.0) / (32767.0 * 2.0);
+        printf("%f\n", r);
     }
 }
 
@@ -46,6 +108,15 @@ void init() {
 
 void cleanup() {
     endwin();
+}
+
+void display(int v) {
+    float r = (v + 32767.0) / (32767.0 * 2.0);
+    int count = (int) (r * win_width);
+    for(int i = 0; i < count; i++){
+        printf("#");
+    }
+    printf("\n");
 }
 
 int go() {
@@ -62,10 +133,13 @@ int go() {
 
     while (fread(buf, sizeof(short), BSZ, ptr_file) > 0) {
         for (int c = 0; c < BSZ; c++) {
-            short cur = buf[c];
-            cb_write(&cb, cur);
+            int cur = (int)buf[c];
+            if(c % 128 == 0) {
+                //cb_write(&cb, (int)cur);
+                display(cur);
+            }
         }
-        cb_debug(&cb);
+        //cb_debug(&cb);
     }
 
     fclose(ptr_file);
@@ -73,6 +147,9 @@ int go() {
 }
 
 int main() {
+    //init();
+    getdims();
     go();
+    //cleanup();
 }
 
