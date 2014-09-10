49570032611f4621508a3963cedcee2e3173420b
jz <jeffz@eml.cc>
2014-09-09 20:29:52 -0600

    checkpoint crap

--- mpdvz.c
+++ mpdvz.c
@@ -1,10 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include <ncurses.h>
+
+
+typedef struct {
+    int size;
+    int start;
+    int end;
+    short *buf;
+} CBuf;
+
+
+void cb_init(CBuf *buf, int size){
+    buf->size = size + 1;
+    buf->start = 0;
+    buf->end = 0;
+    buf->buf = calloc(buf->size, sizeof(short));
+}
+
+void cb_write(CBuf *buf, short val){
+    buf->buf[buf->end] = val;
+    buf->end = (buf->end + 1) % buf->size;
+    if(buf->end == buf->start){
+        buf->start = (buf->start + 1) % buf->size;
+    }
+}
+
+short cb_read(CBuf *buf, int idx){
+    return buf->buf[(buf->start + idx) % buf->size];
+}
+
+void cb_debug(CBuf *buf){
+    for(int i = 0; i < buf->size - 1; i++){
+        printf("%d\n", cb_read(buf, i));
+    }
+}
+
+void buf_test() {
+    CBuf cb;
+    cb_init(&cb, 100);
+
+    for(int i = 10; i < 160; i++){
+        cb_write(&cb, i);
+    }
+
+    cb_debug(&cb);
+
+    printf("%d %d %d", cb.size, cb.start, cb.end);
+}
 
 void init(){
     int rows, cols;
     initscr();
     getmaxyx(stdscr, rows, cols);
+
 
 
     refresh();
@@ -14,12 +64,14 @@ void cleanup() {
     endwin();
 }
 
-int main() {
-    init();
-
+int go() {
     FILE *ptr_file;
     int BSZ = 1024;
     short buf[BSZ];
+
+    CBuf cb;
+    cb_init(&cb, 100);
+
 
     ptr_file = fopen("/tmp/mpd.fifo", "rb");
     if (!ptr_file)
@@ -28,16 +80,19 @@ int main() {
     while (fread(buf, sizeof(short), BSZ, ptr_file) > 0) {
         for (int c = 0; c < BSZ; c++) {
             short cur = buf[c];
-            if (c % 64 == 0)
-            printf(" %d", cur);
-            if (c % 4 == 3) printf("  ");
-            if (c % 16 == 15) printf("\n");
+            cb_write(&cb, cur);
         }
+        cb_debug(&cb);
     }
 
     fclose(ptr_file);
-
-    cleanup();
     return  0;
+}
+
+int main() {
+    buf_test();
+    //init();
+    go();
+    //cleanup();
 }
 
